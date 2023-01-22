c37f5b98304d8d4f7c7f743bc449177368e17a06
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-22 17:32:22 +0800

    feat: add logic for run() in main.go
    run validates user input, processes the csv files specified in
    flag.Args() and then uses csv2float() to created a consolidated slice of
    floats from the column specified, then uses statsFunc() on it to print
    the output

--- main.go
+++ main.go
@@ -30,6 +30,44 @@ func main() {
 	}
 }
 
-func run(fileNames []string, c config, out io.Writer) error {
-	return nil
+func run(fileNames []string, cfg config, out io.Writer) error {
+	var opFunc statsFunc
+
+	// Validate user inputs as a QOL for the user
+	if len(fileNames) == 0 {
+		return ErrNoFiles
+	}
+	if cfg.column < 1 {
+		return fmt.Errorf("%w: %d", ErrInvalidColumn, cfg.column)
+	}
+	switch cfg.op {
+	case "sum":
+		opFunc = sum
+	case "avg":
+		opFunc = avg
+	default:
+		return fmt.Errorf("%w: %s", ErrInvalidOperation, cfg.op)
+	}
+
+	// process CSV files
+	consolidate := make([]float64, 0)
+	// loop through all files adding their data to consolidate
+	for _, fName := range fileNames {
+		// open file for reading
+		f, err := os.Open(fName)
+		if err != nil {
+			return fmt.Errorf("cannot open file: %w", err)
+		}
+
+		// parse the csv into a slice of float64
+		data, err := csv2Float(f, cfg.column)
+		if err != nil {
+			return err
+		}
+
+		// Append the data to consolidate
+		consolidate = append(consolidate, data...)
+	}
+	_, err := fmt.Fprintln(out, opFunc(consolidate))
+	return err
 }
