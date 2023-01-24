a3e523e217159ffb407b034739fd81279551207e
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-25 06:54:51 +0800

    feat: optimize concurrency pattern in main.go
    instead of scheduling however many files as goroutines which would
    potentially introduce a scheduling contention problem, we create worker
    goroutines per available cpu

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
 	"sync"
 )
 
@@ -57,37 +58,47 @@ func run(fileNames []string, cfg config, out io.Writer) error {
 	resultsCh := make(chan []float64)
 	errorCh := make(chan error)
 	doneCh := make(chan struct{})
+	filesCh := make(chan string)
+
+	go func() {
+		defer close(filesCh)
+		for _, fName := range fileNames {
+			filesCh <- fName
+		}
+	}()
 
 	wg := sync.WaitGroup{}
 
 	// loop through data concurrently to be consolidated as soon as
 	// resultsCh is written too which should mean that there were
 	// no errors in parsing the file and writing the data to the resultsCh
-	for _, fName := range fileNames {
+	for i := 0; i < runtime.NumCPU(); i++ {
 		wg.Add(1)
 
-		go func(fName string) {
+		go func() {
 			defer wg.Done()
 
 			// open file for reading
-			f, err := os.Open(fName)
-			if err != nil {
-				errorCh <- fmt.Errorf("cannot open file: %w", err)
-				return
-			}
+			for fName := range filesCh {
+				f, err := os.Open(fName)
+				if err != nil {
+					errorCh <- fmt.Errorf("cannot open file: %w", err)
+					return
+				}
 
-			// parse the csv into a slice of float64
-			data, err := csv2Float(f, cfg.column)
-			if err != nil {
-				errorCh <- err
-			}
+				// parse the csv into a slice of float64
+				data, err := csv2Float(f, cfg.column)
+				if err != nil {
+					errorCh <- err
+				}
 
-			if err := f.Close(); err != nil {
-				errorCh <- err
-			}
+				if err := f.Close(); err != nil {
+					errorCh <- err
+				}
 
-			resultsCh <- data
-		}(fName)
+				resultsCh <- data
+			}
+		}()
 	}
 
 	go func() {
