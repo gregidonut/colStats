1eb4a8940e7d29a31fef258685a898e53c6e8347
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-25 06:09:24 +0800

    feat: implement concurency in parsing csv files
    instead of doing the files one by one this should improve the programs
    benchmarks

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 type config struct {
@@ -51,23 +52,58 @@ func run(fileNames []string, cfg config, out io.Writer) error {
 
 	// process CSV files
 	consolidate := make([]float64, 0)
-	// loop through all files adding their data to consolidate
-	for _, fName := range fileNames {
-		// open file for reading
-		f, err := os.Open(fName)
-		if err != nil {
-			return fmt.Errorf("cannot open file: %w", err)
-		}
 
-		// parse the csv into a slice of float64
-		data, err := csv2Float(f, cfg.column)
-		if err != nil {
+	// Create the channel to receive results or errors of operations
+	resultsCh := make(chan []float64)
+	errorCh := make(chan error)
+	doneCh := make(chan struct{})
+
+	wg := sync.WaitGroup{}
+
+	// loop through data concurrently to be consolidated as soon as
+	// resultsCh is written too which should mean that there were
+	// no errors in parsing the file and writing the data to the resultsCh
+	for _, fName := range fileNames {
+		wg.Add(1)
+
+		go func(fName string) {
+			defer wg.Done()
+
+			// open file for reading
+			f, err := os.Open(fName)
+			if err != nil {
+				errorCh <- fmt.Errorf("cannot open file: %w", err)
+				return
+			}
+
+			// parse the csv into a slice of float64
+			data, err := csv2Float(f, cfg.column)
+			if err != nil {
+				errorCh <- err
+			}
+
+			if err := f.Close(); err != nil {
+				errorCh <- err
+			}
+
+			resultsCh <- data
+		}(fName)
+	}
+
+	go func() {
+		wg.Wait()
+		close(doneCh)
+	}()
+
+	for {
+		select {
+		case err := <-errorCh:
+			return err
+		case data := <-resultsCh:
+			consolidate = append(consolidate, data...)
+		case <-doneCh:
+			_, err := fmt.Fprintln(out, opFunc(consolidate))
 			return err
 		}
-
-		// Append the data to consolidate
-		consolidate = append(consolidate, data...)
 	}
-	_, err := fmt.Fprintln(out, opFunc(consolidate))
-	return err
 }
