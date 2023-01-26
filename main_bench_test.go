94fb63467a9ebb2cc7967f2f171904aedb83e0c6
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-26 16:48:51 +0800

    test: add benchmark test cases for min & max
    to see if we can improve performance to implement this easily i had to
    change the implementation of BenchmarkRun() into a table test using the
    opration to invoke to be the name of the test

--- main_bench_test.go
+++ main_bench_test.go
@@ -7,15 +7,33 @@ import (
 )
 
 func BenchmarkRun(b *testing.B) {
-	fileNames, err := filepath.Glob("./testdata/benchmark/*csv")
-	if err != nil {
-		b.Fatal(err)
+	tests := []struct {
+		operation string
+	}{
+		{
+			operation: "avg",
+		},
+		{
+			operation: "max",
+		},
+		{
+			operation: "min",
+		},
 	}
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		if err := run(fileNames, config{op: "avg", column: 2}, io.Discard); err != nil {
-			b.Error(err)
-		}
+	for _, tt := range tests {
+		b.Run(tt.operation, func(b *testing.B) {
+			fileNames, err := filepath.Glob("./testdata/benchmark/*csv")
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				if err := run(fileNames, config{op: tt.operation, column: 2}, io.Discard); err != nil {
+					b.Error(err)
+				}
+			}
+		})
 	}
 }
